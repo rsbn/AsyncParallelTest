05c892166a6d3d08f8cb08eddaa408167fa672b7
rsbn <paulo@rsbn.com.br>
2019-03-19 20:55:33 -0300

    Initial

---
+++ WinFormUI/frmMain.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WinFormUI
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNormalCall = new System.Windows.Forms.Button();
+            this.btnAsyncCall = new System.Windows.Forms.Button();
+            this.btnParallelCall = new System.Windows.Forms.Button();
+            this.ricLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnNormalCall
+            // 
+            this.btnNormalCall.Location = new System.Drawing.Point(12, 12);
+            this.btnNormalCall.Name = "btnNormalCall";
+            this.btnNormalCall.Size = new System.Drawing.Size(221, 55);
+            this.btnNormalCall.TabIndex = 0;
+            this.btnNormalCall.Text = "Normal call";
+            this.btnNormalCall.UseVisualStyleBackColor = true;
+            this.btnNormalCall.Click += new System.EventHandler(this.btnNormalCall_Click);
+            // 
+            // btnAsyncCall
+            // 
+            this.btnAsyncCall.Location = new System.Drawing.Point(12, 73);
+            this.btnAsyncCall.Name = "btnAsyncCall";
+            this.btnAsyncCall.Size = new System.Drawing.Size(221, 55);
+            this.btnAsyncCall.TabIndex = 1;
+            this.btnAsyncCall.Text = "Async call";
+            this.btnAsyncCall.UseVisualStyleBackColor = true;
+            this.btnAsyncCall.Click += new System.EventHandler(this.btnAsyncCall_ClickAsync);
+            // 
+            // btnParallelCall
+            // 
+            this.btnParallelCall.Location = new System.Drawing.Point(12, 134);
+            this.btnParallelCall.Name = "btnParallelCall";
+            this.btnParallelCall.Size = new System.Drawing.Size(221, 55);
+            this.btnParallelCall.TabIndex = 2;
+            this.btnParallelCall.Text = "Parallel call";
+            this.btnParallelCall.UseVisualStyleBackColor = true;
+            this.btnParallelCall.Click += new System.EventHandler(this.btnParallelCall_Click);
+            // 
+            // ricLog
+            // 
+            this.ricLog.Location = new System.Drawing.Point(239, 12);
+            this.ricLog.Name = "ricLog";
+            this.ricLog.Size = new System.Drawing.Size(280, 177);
+            this.ricLog.TabIndex = 3;
+            this.ricLog.Text = "";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 203);
+            this.Controls.Add(this.ricLog);
+            this.Controls.Add(this.btnParallelCall);
+            this.Controls.Add(this.btnAsyncCall);
+            this.Controls.Add(this.btnNormalCall);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ASync & Parallel test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNormalCall;
+        private System.Windows.Forms.Button btnAsyncCall;
+        private System.Windows.Forms.Button btnParallelCall;
+        private System.Windows.Forms.RichTextBox ricLog;
+    }
+}
+
---
+++ WinFormUI/frmMain.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormUI
+{
+    public partial class frmMain : Form
+    {
+        int time1 = 300;
+        int time2 = 200;
+        int time3 = 150;
+        
+        public frmMain()
+        {
+            InitializeComponent();
+            Worker.log = ricLog;
+        }
+
+        private void btnNormalCall_Click(object sender, EventArgs e)
+        {
+            Worker.ClearLog();
+            Worker.Log("Go Normal!");
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            DoNormalCall();
+            watch.Stop();
+            Worker.Log($"{watch.ElapsedMilliseconds} millisecs");
+        }
+
+        private void DoNormalCall()
+        {
+            Worker.DoWork(time1);
+            Worker.DoWork(time2);
+            Worker.DoWork(time3);
+        }
+
+        private  async Task DoCallAsync()
+        {
+            await Task.Run(()=> Worker.DoWork(time1));
+            await Task.Run(() => Worker.DoWork(time2));
+            await Task.Run(() => Worker.DoWork(time3));
+        }
+
+        private async Task DoCallAsyncParallel()
+        {
+            List<Task> tasks = new List<Task>();
+            tasks.Add(Task.Run(() => Worker.DoWork(time1)));
+            tasks.Add(Task.Run(() => Worker.DoWork(time2)));
+            tasks.Add(Task.Run(() => Worker.DoWork(time3)));
+            await Task.WhenAll(tasks);
+        }
+
+        private async void btnAsyncCall_ClickAsync(object sender, EventArgs e)
+        {
+            Worker.ClearLog();
+            Worker.Log("Go Async!");
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            await DoCallAsync();
+            watch.Stop();
+            Worker.Log($"{watch.ElapsedMilliseconds} millisecs");
+        }
+
+        private async void btnParallelCall_Click(object sender, EventArgs e)
+        {
+            Worker.ClearLog();
+            Worker.Log("Go AsyncParallel!");
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            await DoCallAsyncParallel();
+            watch.Stop();
+            Worker.Log($"{watch.ElapsedMilliseconds} millisecs");
+        }
+    }
+
+    public class Worker
+    {
+        public static RichTextBox log { get; set; }
+        public static void DoWork(int x)
+        {
+            Log($"Starting counting to {x}");
+            for (int i = 0; i < x; i++)
+            {
+                Thread.Sleep(10);
+            }
+            Log($"Task counting to {x} completed.");
+        }
+        public  static void Log(string v)
+        {
+            string msg = "\n" + v;
+                log.BeginInvoke(new MethodInvoker(() => { log.Text += msg; }));
+        }
+        public static void ClearLog()
+        {
+            log.BeginInvoke(new MethodInvoker(() => { log.Text = string.Empty; }));
+        }
+    }
+
+
+}
